58e0b241263930b2e2a24b9ad4b10cdee5c1f363
Abinaya02032000 <153981319+Abinaya02032000@users.noreply.github.com>
2024-02-16 00:51:58 +0530

    python changes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -11,10 +10,11 @@ class CarMake(models.Model):
     color = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.name 
+        return self.name
+
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -34,6 +34,6 @@ class CarModel(models.Model):
             MaxValueValidator(5000000),
             MinValueValidator(100000)
         ])
-        
+
     def __str__(self):
         return self.name  # Return the name as the string representation
